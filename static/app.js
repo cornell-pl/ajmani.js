0e69402f9cf62dfa63a6d55fca3c41442ce4b4a0
Satvik Chauhan <satvikchauhan@rrsdhcp-10-32-37-226.redrover.cornell.edu>
2013-05-14 13:27:28 -0400

    In transit to implement client side support

--- static/app.js
+++ static/app.js
@@ -1,26 +1,19 @@
 (function(){
     'use strict';
     var db = new Database();
-    var t = new Table();
-    t.headers = ['headers','body'];
-    t.createRecord(['From:Nate','hi']);
-    t.createRecord(['From:Raghu','Hello']);
-    t.createRecord(['From:Satvik','Yupppppp']);
-    db.putTable('emails',t);
     $(function(){
 	    var tableTemplate = _.template($('#tableTemplate').html()),
 		tableNameTemplate = _.template($('#tableNameTemplate').html()),
 		viewport = $('#viewport'),
 		viewport2 = $('#viewport2'),
-		renderTable = function(name) {
-		var t = db.getTableByName(name);
+		renderTable = function(t) {
                 if (t !== null) {viewport.empty().append(tableTemplate({table : t}));}
 		else {viewport.empty()};
 	    },
 		getTableName = function() {
 		    viewport2.empty().append(tableNameTemplate({})).find('#formTableName').submit(function(e){
 			    var tName = $('#tableName').val();
-			    renderTable(tName);
+			    db.getTableByName(name,renderTable);
 			    return false;
 			});
 		};
