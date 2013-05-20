a84542a58979288f915ce8ccd578cbcec5611cf2
Raghu <rajkumar@cs.cornell.edu>
2013-05-20 15:50:35 -0400

    Append and split fix

--- SymLens/Map.hs
+++ SymLens/Map.hs
@@ -5,7 +5,7 @@ import SymLens
 import Data.Maybe (fromMaybe, fromJust)
 import qualified Data.Map as Map
 
-appendInto :: (Enum k, Ord k) => SymLens (Map.Map k a, Map.Map k a) (Map.Map k a)
+appendInto :: (Enum k, Ord k, Show k) => SymLens (Map.Map k a, Map.Map k a) (Map.Map k a)
 appendInto = 
   SymLens Map.empty
           (\(m1, m2) mc -> fst $ Map.foldlWithKey (\((m, mc'), nextkey) k a -> ((Map.insert nextkey a m, 
@@ -14,14 +14,14 @@ appendInto =
                                                   ((m1, Map.empty), succ $ fst $ Map.findMax m1) m2)
           (\m mc -> ((Map.difference m mc, Map.mapKeys (fromJust . flip Map.lookup mc) $ Map.intersection m mc), mc))  
                                           
-insert :: (Ord k) => k -> a -> SymLens (Map.Map k a) (Map.Map k a)
+insert :: (Ord k, Eq a, Show a) => k -> a -> SymLens (Map.Map k a) (Map.Map k a)
 insert k initA = 
   SymLens (initA, Nothing) pr pl
   where pr m (a,_) = (Map.insert k a m, (a, Map.lookup k m))
         pl m (a,Nothing) = (Map.delete k m, (fromMaybe a $ Map.lookup k m, Nothing))
         pl m (a,Just a') = (Map.insert k a' m, (fromMaybe a $ Map.lookup k m, Just a'))
 
-fmmap :: (Ord k) => SymLens a b -> SymLens (Map.Map k a) (Map.Map k b)
+fmmap :: (Ord k, Show k) => SymLens a b -> SymLens (Map.Map k a) (Map.Map k b)
 fmmap (SymLens i pr pl) =
   SymLens Map.empty
           (\ma mc -> Map.foldlWithKey (\(mb, mc') k a -> let (b,c) = pr a $ fromMaybe i $ Map.lookup k mc in
