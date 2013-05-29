1907bfaa8dc31e7d26cb4c74ba9aa330a34ed81f
Satvik Chauhan <mystic.satvik@gmail.com>
2013-05-29 12:33:23 -0400

    from and to table working

--- SymLens/Database.hs
+++ SymLens/Database.hs
@@ -2,7 +2,7 @@
 
 module SymLens.Database where
 
-import SymLens 
+import SymLens
 import qualified Data.Map as Map
 import Data.Map (Map)
 import qualified Data.Bimap as Bimap
@@ -12,6 +12,7 @@ import Data.List
 import Control.Monad.Random
 import Data.Char
 import Control.Monad
+import Data.List.Utils
 
 import Database.HDBC
 import Database.HDBC.Sqlite3
@@ -24,15 +25,17 @@ type DatabaseLens = SymLens Conn Conn
 -- liftTableLens :: Name -> TableLens -> DatabaseLens
 -- liftTableLens n (SymLens s pr pl) = SymLens s (doit pr) (doit pl)
 --   where doit put d c = T.swap $ Map.mapAccumWithKey onT c d
---           where onT c n' t | n == n'   = T.swap $ put t c 
---                            | otherwise = (c,t) 
+--           where onT c n' t | n == n'   = T.swap $ put t c
+--                            | otherwise = (c,t)
 
 -- Assumes no table named temp_temp
 
 type Name = String
 type Header = (SqlValue, [SqlValue])
-data Table = Table [SqlValue] (Map SqlValue [SqlValue])
-
+data Table = Table Name                -- Name from which this table is read
+                   (String,[Header])   -- first stores the create statement as it is and second stores the exact header names along with some other info received from PRAGMA table_info
+                   [[SqlValue]]        -- first value is the rowid for each record
+                   deriving (Show)
 rename :: Name -> Name -> DatabaseLens
 rename n1 n2 = SymLens () put put
   where sigma n | n == n1   = n2
@@ -52,21 +55,21 @@ rename n1 n2 = SymLens () put put
                 run c ("ALTER TABLE " ++ n2 ++ " RENAME TO " ++ n1) []
                              | otherwise = return 0
 
-  
+
 drop :: Name -> DatabaseLens
 drop n = SymLens Nothing pr pl
-  where pr c = do 
+  where pr c = do
           ts <- lift $ getTables c
           lift (dropTable ts) >>= put
           return c
-          where dropTable ts | elem n ts = do 
+          where dropTable ts | elem n ts = do
                   n' <- getUniqueName c
                   run c ("ALTER TABLE " ++ n ++ " RENAME TO " ++ n') []
                   return (Just n')
                              | otherwise = return Nothing
-        pl c = do                    
+        pl c = do
           ts <- lift $ getTables c
-          (Just n') <- get 
+          (Just n') <- get
           lift (undropTable n' ts) >> return c
           where undropTable n' ts | elem n ts = do
                   run c ("DROP TABLE " ++ n) []
@@ -74,7 +77,7 @@ drop n = SymLens Nothing pr pl
                                | otherwise = do
                   run c ("ALTER TABLE " ++ n' ++ " RENAME TO " ++ n) []
 
-                               
+
 --   where pr d = maybe (return d) trans $ Map.lookup n d
 --             where
 --               trans t' = put (Just t') >> return (Map.delete n d)
@@ -92,14 +95,14 @@ drop n = SymLens Nothing pr pl
 --           where trans t' = put (Just t') >> return (Map.delete n d)
 
 -- -- -- Take a pred depending on which the new records will go to n1 or n2.
--- -- -- putr :: Database with (n1,n2) and without n -> Database with n and without (n1,n2) 
+-- -- -- putr :: Database with (n1,n2) and without n -> Database with n and without (n1,n2)
 -- -- -- putl :: Database with n and without (n1,n2) -> Database with (n1,n2) and without n
 
--- -- Compelement is a pair of maps mapping keys from the the two initial tables to the appended table 
--- append :: (Id -> Fields -> Bool) 
---        -> Name 
---        -> Name 
---        -> Name 
+-- -- Compelement is a pair of maps mapping keys from the the two initial tables to the appended table
+-- append :: (Id -> Fields -> Bool)
+--        -> Name
+--        -> Name
+--        -> Name
 --        -> DatabaseLens
 -- append on n1 n2 n = SymLens (Bimap.empty, Bimap.empty) pr pl
 --   where pr d = do
@@ -108,7 +111,7 @@ drop n = SymLens Nothing pr pl
 --             (Just (Table h1 m1), Just (Table h2 m2)) | h1 == h2  -> put (lc',rc') >> return (Map.insert n (Table h1 m') $ Map.delete n1 $ Map.delete n2 d)
 --               where (m', rc', _) = Map.foldlWithKey combine (m, rc, newkey') m2
 --                     (m, lc', newkey') = Map.foldlWithKey combine (Map.empty, lc, newkey) m1
---                     combine (m, c, nextkey) k v = 
+--                     combine (m, c, nextkey) k v =
 --                       case Bimap.lookup k c of
 --                         Just k' -> (Map.insert k' v m, c, nextkey)
 --                         Nothing -> (Map.insert nextkey v m, Bimap.insert k nextkey c, nextkey + 1)
@@ -123,12 +126,12 @@ drop n = SymLens Nothing pr pl
 --                     m2' = Map.mapKeys (fromJust  . flip Bimap.lookupR rc) $ Map.intersection m rcmap
 --                     (m1,m2,(lc',rc'),_) = Map.foldlWithKey combine (m1',m2',(lc,rc),(next1,next2)) rest
 --                     rest = Map.difference (Map.difference m lcmap) rcmap
---                     combine (m1,m2,(lc',rc'),(next1,next2)) k v 
+--                     combine (m1,m2,(lc',rc'),(next1,next2)) k v
 --                       | on k v = (Map.insert next1 v m1, m2, (Bimap.insert next1 k lc', rc'), (next1 + 1, next2))
 --                       | otherwise = (m1, Map.insert next2 v m2, (lc', Bimap.insert next2 k rc'), (next1, next2 + 1))
 --                     next1 = maybe 0 ((+1) . fst . fst) $ Map.maxViewWithKey m1'
 --                     next2 = maybe 0 ((+1) . fst . fst) $ Map.maxViewWithKey m2'
---                     lcmap = Bimap.toMapR lc 
+--                     lcmap = Bimap.toMapR lc
 --                     rcmap = Bimap.toMapR rc
 --             _                                                    -> return d
 
@@ -136,18 +139,18 @@ drop n = SymLens Nothing pr pl
 --           Just (Table h m) -> (Map.insert n1 (Table h m1) $ Map.insert n2 (Table h m2) $ Map.delete n d, (lc', rc'))
 --             where m1' = Map.mapKeys (fromJust  . flip Map.lookup lc) $ Map.intersection m lc
 --                   m2' = Map.mapKeys (fromJust  . flip Map.lookup rc) $ Map.intersection m rc
---                   (m1,m2,(lc',rc'),_) 
---                      = Map.foldlWithKey combine (m1',m2',(lc,rc),(next1,next2)) 
+--                   (m1,m2,(lc',rc'),_)
+--                      = Map.foldlWithKey combine (m1',m2',(lc,rc),(next1,next2))
 --                          $ rest
 --                   rest = Map.difference (Map.difference m lc) rc
---                   combine (m1,m2,(lc',rc'),(next1,next2)) k v 
+--                   combine (m1,m2,(lc',rc'),(next1,next2)) k v
 --                     | on k v = (Map.insert next1 v m1, m2, (Map.insert k next1 lc', rc'), (next1 + 1, next2))
 --                     | otherwise = (m1, Map.insert next2 v m2, (lc', Map.insert k next2 rc'), (next1, next2 + 1))
 --                   next1 = maybe 0 ((+1) . fst . fst) $ Map.maxViewWithKey m1'
 --                   next2 = maybe 0 ((+1) . fst . fst) $ Map.maxViewWithKey m2'
-                  
+
 --           _                                                    -> (d,c)
--- -}        
+-- -}
 -- -- | Takes a function to filter the table on and then splits the table
 -- -- into two tables, first satisfying the predicate and other the rest.
 -- -- I doubt this is not a lens. Consider t split on f to t1 and t2. Now
@@ -181,3 +184,44 @@ rndString n = do
 hasTable :: Conn -> Name -> IO Bool
 hasTable c n = getTables c >>= return . elem n
 
+-- assumes that this table is not in the database
+createTable :: Conn -> Name -> Table -> IO ()
+createTable c n (Table n' (h,hs) rs) = do
+  ctb' <- ctb
+  execute ctb' []
+  insert' <- insert
+  sequence_ (map (execute insert') rs)
+  commit c
+  where ctb = prepare c $ copyCreateStatement h n' n
+        insert = prepare c $ "INSERT INTO " ++ n ++ "( rowid,"  ++ concat (intersperse "," (map (\(a,_) -> fromSql a) hs)) ++
+                                                    ") VALUES (?," ++ concat (intersperse "," (map (\_ -> "?") hs)) ++ " )"
+
+readTable :: Conn -> Name -> IO Table
+readTable c n = do
+  ((h:_):_) <- quickQuery c "SELECT sql FROM sqlite_master WHERE type=\'table\' AND name=?" [toSql n]
+  hs' <- quickQuery c  ("PRAGMA table_info(" ++ n ++ ")") []
+  let hs = map fromTableColumn hs'
+  rs <-  quickQuery c ("SELECT rowid,"  ++ concat (intersperse "," (map (\(a,_) -> fromSql a) hs)) ++ " FROM " ++ n) []
+  return (Table n (fromSql h,hs) rs) 
+
+fromTableColumn :: [SqlValue] -> (SqlValue,[SqlValue])
+fromTableColumn (_:n:rs) = (n,rs) -- _ is the key of PRAGMA
+
+toTableColumn :: (SqlValue, [SqlValue]) -> String
+toTableColumn (v,[dt,isNull,null,pk]) = concat $ intersperse " "
+                                          [ fromSql v
+                                          , fromSql dt
+                                          , if (fromSql isNull == "1") then "NOT NULL" else case null of
+                                            SqlNull -> "DEFAULT NULL"
+                                            _       -> fromSql null
+                                          , if (fromSql pk /= "0") then "PRIMARY KEY" else ""  -- problem for multiple primary keys
+                                          ]
+
+copyCreateStatement :: String -> String -> String -> String
+copyCreateStatement q from to = replace (" " ++ from) (" " ++ to) q
+
+copyTableStructure :: Conn -> Name -> Name -> IO ()
+copyTableStructure c from to = do
+  ((a:_):_) <- quickQuery' c "SELECT sql FROM sqlite_master WHERE type=\'table\' AND name=?" [toSql from]
+  runRaw c $ copyCreateStatement (fromSql a) from to
+  return ()
